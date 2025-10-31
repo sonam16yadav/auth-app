c4dffa80a8306a81e214a326d790fd51b5315a5a
sonam16yadav <sonamy1609@gmail.com>
2025-10-31 21:07:55 +0530

    Added VITE_API_URL env config for API connection

--- fronted/src/api/axiosClient.ts
+++ fronted/src/api/axiosClient.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const API_BASE_URL =
   import.meta.env.MODE === "development"
-    ? "http://localhost:3000/api/auth" // when running locally
-    : "https://auth-app-045n.onrender.com/api/auth"; // your Render backend
+    ? "http://localhost:3000/api/auth" // 👩‍💻 When running locally
+    : import.meta.env.VITE_API_URL + "/api/auth"; // 🌐 When deployed (from Vercel env variable)
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
